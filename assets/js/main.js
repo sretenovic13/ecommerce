e9be5a8b6979a719ebea39cfeea0f0ac1ad4d7e6
Stefan Sretenovic <sretenovic.stefan@yahoo.com>
2024-03-14 19:52:04 +0100

    orders amount

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,30 @@ const showOrders = x => {
                 <p styles="font-size: 20px">
                     OrderID ${resultOrder.id}<br/><br/>
                 </p>
+                ${resultOrderDetails.map(y => {
+                    suma += y.quantity * y.unitPrice * (1 - y.discount)
+                    ukupnaSuma += suma 
+                    return `
+                        <p style="font-size: 15px">
+                            ProductId: ${x.productId}<br/><br/>
+                        </p>
+                        <p style="font-size: 15px">
+                            Quantity: ${x.quantity}<br/><br/>
+                        </p>
+                        <p style="font-size: 15px">
+                            UnitPrice: ${x.unitPrice}<br/><br/>
+                        </p>
+                        <p style="font-size: 15px">
+                            Date: ${konacanDatum.slice(0,10)}<hr/>
+                        </p>
+                        <p style="font-size: 15px">
+                            Amount: ${suma.toFixed(2)}<hr/>
+                        </p>
+                    `
+                }).join(',')}
+                <p style="font-size: 15px">
+                     Total amount: ${ukupnaSuma.toFixed(2)}<br/>
+                </p>
             </div>
         </div>
     `
