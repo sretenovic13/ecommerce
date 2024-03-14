f8a06e4d4c354e85f5ffeb1736296f7d5f6fd3c6
sretenovic13 <sretenovic.stefan@yahoo.com>
2024-03-14 10:44:08 +0100

    main

---
+++ assets/js/signin.js
@@ -0,0 +1,34 @@
+import { getAuth, signInWithEmailAndPassword,onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-database.js";
+
+import app from './config.js'
+
+const auth = getAuth()
+
+loginBtn.addEventListener('click', e => {
+    const email = document.getElementById('email')
+    const password = document.getElementById('password')
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user
+
+            sessionStorage.setItem('keyUsername', email)
+            sessionStorage.setItem('keyPassword', password)
+            sessionStorage.setItem('daLiJeAktivnaSesija', true)
+            
+            location.href = 'index.html'
+        })
+        .catch((error) => {
+            const errorCode = error.code
+            const errorMessage = error.message 
+
+            if (errorCode === 'auth/internal-error') {
+                $('#messageError').text('Niste uneli podatke')
+            } else if(errorCode === 'auth/invalid-email') {
+                $('#messageError').text('Niste uneli validan email')
+            } else if(errorCode === 'auth/invalid-login-credentials'){
+                $('#messageError').text('Niste uneli validan password')
+            }
+        })
+})
